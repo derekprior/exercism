e817a94cb7e947c46a5466ea7252d0e918c790a6
Derek Prior <derekprior@gmail.com>
2013-10-07 17:51:46 -0400

    Make Bob.js specs pass
    Needs a refactor for clarification and cleanliness.

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,8 +1,8 @@
 var Bob = function() {
   this.hey = function(input) {
-    isSilentTreatment = function(input) { return false; }
-    isShouting = function(input) { return false; }
-    isQuestion = function(input) { return false; }
+    isSilentTreatment = function(input) { return input.length === 0 || /^\s*$/.test(input) }
+    isShouting = function(input) { return input.toUpperCase() === input; }
+    isQuestion = function(input) { return input.charAt(input.length - 1) === '?'; }
 
     if (isSilentTreatment(input)) { return 'Fine. Be that way!'; }
     if (isShouting(input)) { return 'Woah, chill out!'; }
